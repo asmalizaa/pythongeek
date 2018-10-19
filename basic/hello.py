452c0c4a5280486664dfc289cdb3bbc350e9b86d
asmaliza <asmalizaa@gmail.com>
2018-10-19 22:39:54 +0800

    helloworld

---
+++ basic/hello.py
@@ -0,0 +1,10 @@
+"""
+Author: Asmaliza Ahzan
+Email: asmalizaa@gmail.com
+Date: 19th October 2018
+
+Descriptions:
+    1. This is the first python program that displays message "Hello World"
+
+"""
+print("Hello World!")
