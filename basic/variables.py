780dc9a8a465dfac3a9ecaf3178b92f43e66f591
asmaliza <asmalizaa@gmail.com>
2018-10-19 23:01:49 +0800

    variables

---
+++ basic/variables.py
@@ -0,0 +1,22 @@
+"""
+Author: Asmaliza Ahzan
+Email: asmalizaa@gmail.com
+Date: 19th October 2018
+
+Descriptions:
+    1. This script show examples on how to create and use python variables
+
+"""
+# define a variable to store a string
+name = "John Smith"
+
+# define a variable to store a number
+age = 25
+
+# display
+print("Hello", name)
+print("Hello " + name)
+print(name, "'s age is", age)
+print(name + "'s age is " + str(age))
+
+# print() can only take string input so non-string needs to be converted to a string first by using str()
